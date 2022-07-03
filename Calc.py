aad692f4e4418ba56aa61a316d669d39a59426fb
George Wade <103867594+Odindadoggie@users.noreply.github.com>
2022-07-03 10:03:26 -0700

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,17 @@
+import os
+import game
+import unity
+import docker
+import kubernetes
+import keywords
+keywords = calc
+if x < 9:
+  calc_num = calc
+  def numberCalculate():
+    keywords_calc = calc lambda num_123456789
+    numberCalculate()
+   while y = keywords:
+    calc = lambda num_987654321
+    y*9 == 99
+    break y*9
+    
