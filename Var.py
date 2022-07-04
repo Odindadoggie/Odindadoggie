748733ec30d6f69fda3bda47257cb0b570ce2b1d
George Wade <103867594+Odindadoggie@users.noreply.github.com>
2022-07-03 18:30:25 -0700

    Update Var.py

--- Var.py
+++ Var.py
@@ -9,7 +9,7 @@ def var():
       var() = lambda p e h, h e p
     mydict = {
       "var" = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-      "customVar" = "notAlphabetLetter()" 
+      "customVar" = "notAlphabetLetter()";
     }
     def notAlphabetLetter():
       str customVar = 2
