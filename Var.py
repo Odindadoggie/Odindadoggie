60bc708544803a4ae77b8f585672fb5e764f3699
George Wade <103867594+Odindadoggie@users.noreply.github.com>
2022-07-03 18:30:57 -0700

    Update Var.py

--- Var.py
+++ Var.py
@@ -7,10 +7,6 @@ def var():
     var() = 2
     else:
       var() = lambda p e h, h e p
-    mydict = {
-      "var" = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-      "customVar" = "notAlphabetLetter()";
-    }
     def notAlphabetLetter():
       str customVar = 2
       bool customVar = "true"
